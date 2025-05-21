f3855af6c2f949bc9bf6510feb9f136b7b685e01
prasannanjaneya <prasanndevopsr@gmail.com>
2025-05-21 16:48:16 +0200

    added the tfvar and vnet

--- aztf/variable.tf
+++ aztf/variable.tf
@@ -1,8 +1,39 @@
 variable "location" {
   type = string
-  default = "West Europe"
 }
+
 variable "rg" {
   type = string
-  default = "aztf-rg"
+}
+
+variable "vnet_name" {
+  type = string
+}
+
+variable "vnet_address_space" {
+  type = list(string)
+}
+
+variable "subnet_name_0" {
+  type = string
+}
+
+variable "subnet_name_1" {
+  type = string
+}
+
+variable "subnet_name_2" {
+  type = string
+}
+
+variable "subnet_address_prefix_0" {
+  type = string
+}
+
+variable "subnet_address_prefix_1" {
+  type = string
+}
+
+variable "subnet_address_prefix_2" {
+  type = string
 }
---
+++ aztf/vnet.tf
@@ -0,0 +1,27 @@
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.vnet_name
+  address_space       = var.vnet_address_space
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet-0" {
+  name                 = var.subnet_name_0
+  address_prefixes     = [var.subnet_address_prefix_0]
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+}
+
+resource "azurerm_subnet" "subnet-1" {
+  name                 = var.subnet_name_1
+  address_prefixes     = [var.subnet_address_prefix_1]
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+}
+
+resource "azurerm_subnet" "subnet-2" {
+  name                 = var.subnet_name_2
+  address_prefixes     = [var.subnet_address_prefix_2]
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+}
